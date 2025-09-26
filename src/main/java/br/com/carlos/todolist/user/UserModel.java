c1d8d9d2019e69a7cbc147c54e081cba4e2a57f3
Carlos Antonio <gerrilhas@gmail.com>
2025-09-26 18:18:40 -0300

    banco de dados jpa h2

--- src/main/java/br/com/carlos/todolist/user/UserModel.java
+++ src/main/java/br/com/carlos/todolist/user/UserModel.java
@@ -1,34 +1,15 @@
 package br.com.carlos.todolist.user;
 
+import lombok.Data;
 
+@Data
 public class UserModel {
     
     private String username;
     private String name;
     private String password;
 
-    //getters and setters
-
-public void setUsername(String username){
-    this.username= username;
-}
-
-public String getUsername(){
-    return this.username;
-}
-public void setName(String name){
-    this.name= name;
-}
-public String getName(){
-    return this.name;
-}
-public void setPassword(String password){
-    this.password=password;
-}
-public String getPassword(){
-    return this.password;
-}
-
+    
 
 }
 
